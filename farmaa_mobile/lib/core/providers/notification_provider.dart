4f6c5be87f6e58175ffa87b747dcc53fca844ce5
Bharath Raj <bharathraj14120@gmail.com>
2026-03-19 16:18:09 +0530

    Fix: remove unused firebase_messaging import in notification_provider

--- farmaa_mobile/lib/core/providers/notification_provider.dart
+++ farmaa_mobile/lib/core/providers/notification_provider.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import '../../features/shared/screens/notifications_screen.dart'; // To get NotificationItem and NotificationType
 import '../services/notification_service.dart';
 
